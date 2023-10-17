96667045ae14283e3f480680ce70a02db49ee17f
Daniela Gordenko <DGordenko@MARUPE.EDU.LV>
2023-10-17 16:12:01 +0300

    add. php
    pievienojam datu bazi un add php

---
+++ add.php
@@ -0,0 +1,9 @@
+<?php
+$v = $_POST["vards"];
+$u = $_POST["uzvards"];
+$l = $_POST["login"];
+$p = $_POST["password"];
+
+include("cfg.php");
+$mysqli->query("INSERT INTO users('id','name',"" )");
+?>
---
+++ cfg.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "users";
+
+$mysqli = new mysqli($servername,$username,$password,$db);
+?>
